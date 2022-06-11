50e58edc1db5e4b7a41b40795e661a8b9c9ae5b1
Oscar Pena <openav94@gmail.com>
2022-06-11 09:38:43 -0400

    Funcion de tarea completa y eliminar tarea agregada

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+( () => {
 const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
@@ -7,17 +8,49 @@ const createTask = (evento) => {
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
-    const content = ` <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML= content;
+
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerHTML = value;
+    taskContent.appendChild(titleTask);
+
+
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 
-    console.log(content);
 }
 
-console.log(btn);
-
+//Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
+
+const checkComplete = ()=> {
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click",completeTask);
+    return i;
+}
+
+//Immediately invoked function expression IIFE
+const completeTask = (event) => {
+    const element = event.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+}
+
+const deleteIcon = () => {
+    const i = document.createElement("i");
+    i.classList.add("fas","fa-trash-alt","trashIcon","icon");
+    i.addEventListener("click", deleteTask)
+    return i;
+}
+
+const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove(); 
+}
+
+}) ();
